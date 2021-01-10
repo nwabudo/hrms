a32e995f1c1a0020fe06f361def07c28244ab5ef
neoOkpara <emmox55@gmail.com>
2021-01-10 01:29:23 +0100

    initial commit

---
+++ src/test/java/io/neoOkpara/ws/hr/HRMSApplicationTests.java
@@ -0,0 +1,13 @@
+package io.neoOkpara.ws.hr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HRMSApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
