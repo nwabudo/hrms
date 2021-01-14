5b94e7721a3dcf0301c7cd8a4874ef68e2941087
neoOkpara <emmox55@gmail.com>
2021-01-14 12:37:04 +0100

    Adjusted the Class for maintainBenefit()

--- src/main/java/io/neoOkpara/ws/hr/entiy/user/Benefits.java
+++ src/main/java/io/neoOkpara/ws/hr/entiy/user/Benefits.java
@@ -2,35 +2,22 @@ package io.neoOkpara.ws.hr.entiy.user;
 
 import java.math.BigDecimal;
 
-import org.bson.types.ObjectId;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Field;
-
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-//@Document(collection = "benefit_tbl")
-@Builder
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
 public class Benefits {
-
-	@Id
-	private ObjectId _id;
 	
-	@Field(name = "salary_amount")
 	private BigDecimal salaryAmount;
 	
-	@Field(name = "vacation_amount")
 	private BigDecimal vacationAmount;
 	
-	@Field(name = "annual_bonus")
 	private BigDecimal annualBonus;
 }
